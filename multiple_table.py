73ed045c258196461e2840df71ec4414a850b73a
wanjin <yuwanjin70@gmail.com>
2022-11-08 11:32:32 +0900

    '강의꺼'

--- multiple_table.py
+++ multiple_table.py
@@ -1,12 +1,6 @@
 print("구구단 몇단을 계산할까요?")
-num = int(input())
+dan = int(input())
 
-print(f"구구단 {num}단을 계산합니다.")
-
-i = 1
-while i < 10:
-    mul = num * i
-    result= (f"{num} x {i} = {mul}")
-    print(result)
-    i = i + 1 
-
+print(f"구구단 {dan}단을 계산합니다.")
+for i in range(1, 10):
+    print(f"{dan} X {i} = {dan*i} ")
