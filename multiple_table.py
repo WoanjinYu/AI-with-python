2e698ef109e80dcdb4348dd26bff2049d4665581
yuwanjin <yuwanjin70@gmail.com>
2022-11-16 11:02:29 +0900

    '구구단연속계산기능수정'

--- multiple_table.py
+++ multiple_table.py
@@ -1,6 +1,14 @@
-print("구구단 몇단을 계산할까요?")
+print("구구단 몇단을 계산할까요? (1~9)")
+
 dan = int(input())
 
-print(f"구구단 {dan}단을 계산합니다.")
-for i in range(1, 10):
-    print(f"{dan} X {i} = {dan*i} ")
+while (dan != 0):
+    if dan >= 10:
+        print("1부터 9사이의 숫자만 입력해주세요.\n")
+    else: 
+        print(f"구구단 {dan}단을 계산합니다.")
+        for i in range(1, 10):
+            print(f"{dan} X {i} = {dan*i} ")
+    print("구구단 몇단을 계산할까요? (1~9)")
+    dan = int(input())
+else: print("구구단 게임을 종료합니다.")
