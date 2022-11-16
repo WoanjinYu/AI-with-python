5eb11bf7802c9541d2711083422f4ea05c103548
yuwanjin <yuwanjin70@gmail.com>
2022-11-16 10:37:19 +0900

    '100이상일때,소수점이있을때판별문추가'

--- guess_number_ex.py
+++ guess_number_ex.py
@@ -1,12 +1,16 @@
 import random
 gu_num = random.randint(1,100)
 print("숫자를 맞춰 보세요 (1~100)")
-in_num = int(input())
+in_num = float(input())
 
 while(gu_num is not in_num):
-    if in_num > gu_num:
+    if in_num > 100:
+        print("숫자는 100을 넘지 않습니다")
+    elif in_num is not int:
+        print("숫자는 자연수입니다")
+    elif in_num > gu_num:
         print("숫자가 너무 큽니다.")
-    else:
+    elif in_num < gu_num:
         print("숫자가 너무 작습니다.")
     in_num = int(input())
 else: print(f"정답입니다. 숫자는 {gu_num}입니다.")
