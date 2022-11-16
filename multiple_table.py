5f4ab25c60e41c09ac0d85380f4066439078f13d
yuwanjin <yuwanjin70@gmail.com>
2022-11-16 15:29:28 +0900

    '강의영상참고'

--- multiple_table.py
+++ multiple_table.py
@@ -1,14 +1,13 @@
 print("구구단 몇단을 계산할까요? (1~9)")
-
-dan = int(input())
-
-while (dan != 0):
-    if dan >= 10:
-        print("1부터 9사이의 숫자만 입력해주세요.\n")
-    else: 
-        print(f"구구단 {dan}단을 계산합니다.")
-        for i in range(1, 10):
-            print(f"{dan} X {i} = {dan*i} ")
-    print("구구단 몇단을 계산할까요? (1~9)")
-    dan = int(input())
-else: print("구구단 게임을 종료합니다.")
+x=1
+while( x is not 0):
+    x =int(input())
+    if x == 0: break
+    if not(1<= x <= 9):
+        print("잘못 입력하셨습니다", "1부터 9사이  숫자를 입력해주세요.")
+    else:
+        print("구구단" + str(x)+ "단을 계산합니다.")
+        for i in range(1,10):
+            print(str(x)+ "X" + str(i) + "=" + str(x*i))
+        print("구구단 몇단을 계산할까요? (1~9)")
+print("구구단 게임을 종료합니다.")
