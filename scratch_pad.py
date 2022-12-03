5782e1a8649f36f09238fc20dcf27839fe027ed7
yuwanjin <yuwanjin70@gmail.com>
2022-12-03 20:37:43 +0900

    .

--- scratch_pad.py
+++ scratch_pad.py
@@ -101,22 +101,18 @@
 # h += m//60
 # print(h%24,m%60)
 
-#주사위 세개
-# a, b, c = map(int, input().split())
-# if a == b == c:
-#     print(10000 + a * 1000)
-# elif a == b or b == c or a == c:
-#     if a == b:
-#         print(1000 + a * 100)
-#     elif b == c:
-#         print(1000 + b * 100)
-#     else: 
-#         print(1000 + c * 100)
-# else:
-#     if  a > b and a > c :
-#         print(a * 100)
-#     elif b > a and b > c:
-#         print(b * 100)
-#     else :
-#         print(c * 100)
+# 주사위 세개
+a, b, c = map(int, input().split())
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b or b == c or a == c:
+    if a == b:
+        print(1000 + a * 100)
+    elif b == c:
+        print(1000 + b * 100)
+    else: 
+        print(1000 + c * 100)
+else:
+    M = [a, b, c]
+    print(max(M) * 100)
             
