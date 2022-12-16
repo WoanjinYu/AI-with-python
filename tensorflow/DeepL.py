b5a0195523bf9de8c30b7d4483fe379538c539b4
yuwanjin <yuwanjin70@gmail.com>
2022-12-16 17:51:24 +0900

    2022.12.15

--- tensorflow/DeepL.py
+++ tensorflow/DeepL.py
@@ -4,8 +4,17 @@ data = pd.read_csv('gpascore.csv')
 
 
 # print(data.isnull().sum()) 
-data = data.dropna()
-data.fillna(100) # 빈칸을 (값) 만큼 채워줌 fillna(값)
+data = data.dropna() # 빈칸 없애기
+# data.fillna(100) # 빈칸을 (값) 만큼 채워줌 fillna(값)
+# print(data['gpa'].max()) #최대값 구하기
+
+y데이터  = data['admit'].values #'admit' 세로열에 있는 데이터 출력해줌
+
+x데이터 = []
+
+for i, rows in data.iterrows():
+    print(rows['gre']) #'gre'세로 옆에 있는 데이터 출력해줌
+
 
 
 exit()
